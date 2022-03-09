3288a631f611abf2fadd369eb865155300a57432
italo-freitas <101228245+italo-freitas@users.noreply.github.com>
2022-03-08 22:15:23 -0300

    Add files via upload

---
+++ project2/EmpregadoDaFaculdade.java
@@ -0,0 +1,19 @@
+package project2;
+
+public class EmpregadoDaFaculdade {
+	private String nome;
+	private double salario;
+	private int horasAula;
+	
+	double getgastos() {
+		return this.salario;
+		double salario;
+		salario = (horasAula * 40);
+		
+	}
+	String getInfo() {
+		return "nome:" + this.nome + "com salario" + this.salario;
+		
+	}
+
+}
